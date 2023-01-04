5b6e99488910d5e9f997996199f3d16249568284
Okan Akdag <mertakdag08@gmail.com>
2023-01-04 21:35:39 +0300

    Create Vertex class

---
+++ Vertex.cs
@@ -0,0 +1,11 @@
+public class Vertex
+{
+    private char label;
+    
+    public Vertex(char label)
+    {
+        this.Label = label;
+    }
+
+    public char Label { get => label; set => label = value; }
+}
